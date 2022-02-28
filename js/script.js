50e62b181f589809e2fc7ecf497d07046bf07160
Gaurav Solanki <gsolanki@ex2india.com>
2022-02-28 16:05:45 +0530

    ..

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 var tl = new TimelineMax({ onUpdate: updatePercentage });
-var controller = new ScrollMagic.Controller();
+var controller = new ScrollMagic.Controller({
+    globalSceneOptions:
+    {
+        triggerHook: 'onLeave'
+    }
+});
 
 tl.from(".s-2-inner", 1, { y: -300, opacity: 0 });
 tl.from(".s-2-inner h2", 1, { x: 200, opacity: 0 });
@@ -7,16 +12,20 @@ tl.from(".s-2-inner p", 1, { x: -200, opacity: 0 });
 tl.from(".s-2-inner a", 1, { x: 100, opacity: 0 });
 tl.from(".explore-img", 1, { x: 50, opacity: 0 });
 
-const scene = new ScrollMagic.Scene({
-  triggerElement: ".explore-wrap",
-  triggerHook: "onLeave",
-  duration: "100%"
-})
-  .setPin(".explore-wrap")
-  .setTween(tl)
-  .addTo(controller);
+var scene = document.querySelectorAll('.explore-wrap');
 
-function updatePercentage() {
-  tl.progress();
-  console.log(tl.progress());
+for (const section of scene) {
+    const scene = new ScrollMagic.Scene({
+        triggerElement: section,
+    })
+        .setPin(section)
+        .setTween(tl)
+        .addTo(controller);
 }
+
+
+    function updatePercentage() {
+        tl.progress();
+        console.log(tl.progress());
+    }
+
