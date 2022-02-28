45ada88c2c1896dc08189e912e5fa0e13beff4d0
Gaurav Solanki <gsolanki@ex2india.com>
2022-02-28 18:06:29 +0530

    test code

--- js/script.js
+++ js/script.js
@@ -1,31 +1,42 @@
-var tl = new TimelineMax({ onUpdate: updatePercentage });
-var controller = new ScrollMagic.Controller({
-    globalSceneOptions:
-    {
-        triggerHook: 'onLeave'
+var tl = new TimelineMax({onUpdate:updatePercentage});
+// var t2 = new TimelineMax();
+var controller = new ScrollMagic.Controller();
+
+
+var exploreWrap1 = document.querySelector(".explore-wrap-1");
+tl.from(exploreWrap1.querySelector(".s-2-inner"), 1, { y: -300, opacity: 0 });
+tl.from(exploreWrap1.querySelector(".s-2-inner h2"), 1, { x: 200, opacity: 0 });
+tl.from(exploreWrap1.querySelector(".s-2-inner p"), 1, { x: -200, opacity: 0 });
+tl.from(exploreWrap1.querySelector(".s-2-inner a"), 1, { x: 100, opacity: 0 });
+tl.from(exploreWrap1.querySelector(".explore-img"), 1, { x: 50, opacity: 0 });
+
+// var scene = document.querySelectorAll('.explore-wrap');
+
+const scene = new ScrollMagic.Scene({
+    triggerElement: exploreWrap1,
+    triggerHook:"onLeave",
+    duration:"100%"
+})
+    .setPin(exploreWrap1)
+    .setTween(tl)
+    .addTo(controller);
+
+    function updatePercentage(){
+        t1.progress()
     }
-});
+// // second 
 
-tl.from(".s-2-inner", 1, { y: -300, opacity: 0 });
-tl.from(".s-2-inner h2", 1, { x: 200, opacity: 0 });
-tl.from(".s-2-inner p", 1, { x: -200, opacity: 0 });
-tl.from(".s-2-inner a", 1, { x: 100, opacity: 0 });
-tl.from(".explore-img", 1, { x: 50, opacity: 0 });
+// const exploreWrap2 = document.querySelector(".explore-wrap-2");
+// t2.from(exploreWrap2.querySelector(".s-2-inner"), 1, { y: -300, opacity: 0 });
+// t2.from(exploreWrap2.querySelector(".s-2-inner h2"), 1, { x: 200, opacity: 0 });
+// t2.from(exploreWrap2.querySelector(".s-2-inner p"), 1, { x: -200, opacity: 0 });
+// t2.from(exploreWrap2.querySelector(".s-2-inner a"), 1, { x: 100, opacity: 0 });
+// t2.from(exploreWrap2.querySelector(".explore-img"), 1, { x: 50, opacity: 0 });
 
-var scene = document.querySelectorAll('.explore-wrap');
-
-for (const section of scene) {
-    const scene = new ScrollMagic.Scene({
-        triggerElement: section,
-    })
-        .setPin(section)
-        .setTween(tl)
-        .addTo(controller);
-}
-
-
-    function updatePercentage() {
-        tl.progress();
-        console.log(tl.progress());
-    }
+// const scene2 = new ScrollMagic.Scene({
+//     triggerElement: exploreWrap2,
+// })
+//     .setPin(exploreWrap2)
+//     .setTween(t2)
+//     .addTo(controller);
 
