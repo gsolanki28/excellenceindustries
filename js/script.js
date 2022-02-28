aae56c8d424daa17f5f038c6c92e9829550511e2
Gaurav Solanki <gsolanki@ex2india.com>
2022-02-28 15:36:37 +0530

    ..

--- js/script.js
+++ js/script.js
@@ -5,12 +5,7 @@ tl.from(".s-2-inner", 1, { y: -300, opacity: 0 });
 tl.from(".s-2-inner h2", 1, { x: 200, opacity: 0 });
 tl.from(".s-2-inner p", 1, { x: -200, opacity: 0 });
 tl.from(".s-2-inner a", 1, { x: 100, opacity: 0 });
-tl.from(".explore-img-right", 1, { x: 50, opacity: 0 });
-tl.from(".s-3-inner", 1, { y: -300, opacity: 0 });
-tl.from(".s-3-inner h2", 1, { x: 200, opacity: 0 });
-tl.from(".s-3-inner p", 1, { x: -200, opacity: 0 });
-tl.from(".s-3-inner a", 1, { x: 100, opacity: 0 });
-tl.from(".explore-img-left", 1, { x: -50, opacity: 0 });
+tl.from(".explore-img", 1, { x: 50, opacity: 0 });
 
 const scene = new ScrollMagic.Scene({
   triggerElement: ".explore-wrap",
