bd6ca7d4fd3293043cc78ae7b59d9b0017da5344
Gaurav Solanki <gsolanki@ex2india.com>
2022-03-25 15:31:32 +0530

    refactoring

--- src/script.js
+++ src/script.js
@@ -3,53 +3,22 @@ import 'lazysizes';
 import 'lazysizes/plugins/parent-fit/ls.parent-fit';
 import Experience from './Experience/Experience.js';
 
-
-const slideIndicator = document.querySelectorAll('.indicator');
-const allSlides = document.querySelectorAll('.features-item');
-const sliderContainer = document.querySelector('.features-container');
-const hotSpotIndicator = document.querySelectorAll('.features-hotspot');
-
-
-
 let modal360 = document.querySelector('.modal-360');
-    document.querySelector('.explore-360-icon').addEventListener('click', function () {
-      modal360.style.display = "grid";
-      const experience = new Experience(document.querySelector('canvas#product360'));
-    })
+let fullPage = document.querySelector('#fullpage');
+document.querySelector('.explore-360-icon').addEventListener('click', function () {
+    modal360.style.display = "grid";
+    fullPage.style.overflow = "hidden";
+    const experience = new Experience(document.querySelector('canvas#product360'));
+})
 
-    function closeModal() {
-      modal360.style.display = "none";
-    }
-
-    document.querySelector('.modal-close').addEventListener('click', function () {
-      closeModal()
-    })
- 
-const sliderFunction = (e) => {
-    let getSlideId = e.target.dataset.target;
-    slideIndicator.forEach((indicator) => {
-        indicator.classList.remove('active');
-        if (indicator.dataset.target === e.target.dataset.target) {
-            indicator.classList.add('active');
-        }
-    })
-    allSlides.forEach((slide) => {
-        slide.classList.remove('active');
-    })
-    document.querySelector(`[data-id=${getSlideId}]`).classList.add('active');
+function closeModal() {
+    modal360.style.display = "none";
+    fullPage.style.overflow = "initial";
 }
 
-slideIndicator.forEach((indicator) => {
-    indicator.addEventListener('click', (e) => {
-        sliderFunction(e);
-    })
-});
-
-// hotSpotIndicator.forEach((hotspot) => {
-//     hotspot.addEventListener('click', (e) => {
-//         sliderFunction(e);
-//     })
-// })
+document.querySelector('.modal-close').addEventListener('click', function () {
+    closeModal()
+})
 
 
 
