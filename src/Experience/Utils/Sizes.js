83312c78f4a07e7146267407fe5e31e831565c84
Gaurav Solanki <gsolanki@ex2india.com>
2022-03-25 10:19:17 +0530

    size of canvas

--- src/Experience/Utils/Sizes.js
+++ src/Experience/Utils/Sizes.js
@@ -1,19 +1,16 @@
 import EventEmitter from './EventEmitter.js'
 
-export default class Sizes extends EventEmitter
-{
-    constructor()
-    {
+export default class Sizes extends EventEmitter {
+    constructor() {
         super()
 
         // Setup
-        this.width = document.querySelector('.modal-dialog').innerWidth
-        this.height = document.querySelector('.modal-dialog').innerHeight
+        this.width = document.querySelector('.modal-dialog').clientWidth
+        this.height = document.querySelector('.modal-dialog').clientHeight
         this.pixelRatio = Math.min(window.devicePixelRatio, 2)
 
         // Resize event
-        window.addEventListener('resize', () =>
-        {
+        window.addEventListener('resize', () => {
             this.width = document.body.clientWidth
             this.height = 600
             this.pixelRatio = Math.min(window.devicePixelRatio, 2)
