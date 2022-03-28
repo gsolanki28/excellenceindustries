f0c4509dd59c00fb2418ce8a404c8c1e01847bb8
Gaurav Solanki <gsolanki@ex2india.com>
2022-03-28 15:57:18 +0530

    prevent scroll when modal open

--- src/script.js
+++ src/script.js
@@ -7,18 +7,26 @@ let modal360 = document.querySelector('.modal-360');
 let fullPage = document.querySelector('#fullpage');
 document.querySelector('.explore-360-icon').addEventListener('click', function () {
     modal360.style.display = "grid";
-    fullPage.style.overflow = "hidden";
     const experience = new Experience(document.querySelector('canvas#product360'));
 })
 
 function closeModal() {
     modal360.style.display = "none";
-    fullPage.style.overflow = "initial";
 }
 
 document.querySelector('.modal-close').addEventListener('click', function () {
     closeModal()
 })
+    
+    
+var handleEvent = function(e) {
+    e.preventDefault();
+    e.stopPropagation();      // disables scrolling by mouse wheel and touch move
+};
+    
+modal360.addEventListener('scroll', handleEvent, false);
+modal360.addEventListener('wheel', handleEvent, false);
+modal360.addEventListener('touchmove', handleEvent, false);
 
 
 
