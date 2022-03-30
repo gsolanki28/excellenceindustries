3be983b7c93ef781e886389b1a21324c7ef46652
Gaurav Solanki <gsolanki@ex2india.com>
2022-03-30 11:30:35 +0530

    js moved to script

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 import './css/main.scss'
 import 'lazysizes';
 import 'lazysizes/plugins/parent-fit/ls.parent-fit';
+
 import Experience from './Experience/Experience.js';
 
 let modal360 = document.querySelector('.modal-360');
@@ -27,6 +28,68 @@ var handleEvent = function(e) {
 modal360.addEventListener('scroll', handleEvent, false);
 modal360.addEventListener('wheel', handleEvent, false);
 modal360.addEventListener('touchmove', handleEvent, false);
+
+
+
+// full page JS
+
+new fullpage("#fullpage", {
+    //options here
+    //anchors: ["firstPage", "secondPage", "3rdPage", "4thpage"],
+    menu: "#menu",
+    dragAndMove: true,
+    licenseKey: "8EF54198-043049E8-A2559EAF-7849E171",
+    dragAndMoveKey: "ZXhpdGVzdC5jb21fQ09FWkhKaFowRnVaRTF2ZG1VPTgycg==",
+    offsetSections: false,
+    slidesNavigation: true,
+    scrollOverflow: false,
+    onLeave: function (origin, destination, direction) {
+      destination.item.classList.add("lazy-loaded");
+    }
+  });
+
+  const slideIndicator = document.querySelectorAll(".indicator");
+  const allSlides = document.querySelectorAll(".feature");
+  const sliderContainer = document.querySelector(".features-container");
+  const hotSpotIndicator = document.querySelectorAll(".feature-hotspot");
+  document.querySelectorAll(".explore-hotspot").forEach((hotspot) => {
+    hotspot.addEventListener("click", (e) => {
+      sliderFunction(e);
+      fullpage_api.moveTo(3);
+      e.preventDefault();
+    });
+  });
+
+  document
+    .querySelector(".scroll-down")
+    .addEventListener("click", (e) => {
+      fullpage_api.moveTo(2);
+      e.preventDefault();
+    });
+
+  slideIndicator.forEach((indicator) => {
+    indicator.addEventListener("click", (e) => {
+      sliderFunction(e);
+      fullpage_api.moveTo(3);
+    });
+  });
+
+  const sliderFunction = (e) => {
+    let getSlideId = e.target.dataset.target;
+    slideIndicator.forEach((indicator) => {
+      indicator.classList.remove("active");
+      if (indicator.dataset.target === e.target.dataset.target) {
+        indicator.classList.add("active");
+      }
+    });
+    allSlides.forEach((slide) => {
+      slide.classList.remove("active");
+    });
+    document
+      .querySelector(`[data-id=${getSlideId}]`)
+      .classList.add("active");
+  };
+
 
 
 
